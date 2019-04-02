675a9858c7b10d54d648982df705cb276915ca39
Satit Riapit <siteslave@msn.com>
2019-04-02 16:00:42 +0700

    add register page

--- lib/register.dart
+++ lib/register.dart
@@ -12,18 +12,45 @@ class _RegisterPageState extends State<RegisterPage> {
       appBar: AppBar(
         title: Text('Register new user'),
       ),
-      body: ListView(
-        children: <Widget>[
-          Text('User detail'),
-          TextFormField(),
-          TextFormField(),
-          TextFormField(),
-          RaisedButton.icon(
-            icon: Icon(Icons.save),
-            label: Text('REGISTER'),
-            onPressed: () {},
-          )
-        ],
+      body: Padding(
+        padding: const EdgeInsets.all(20.0),
+        child: ListView(
+          children: <Widget>[
+            Text(
+              'ENTER USER INFORMATION',
+              style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
+            ),
+            TextFormField(
+              decoration: InputDecoration(
+                  labelText: 'PERSONAL ID',
+                  border: InputBorder.none,
+                  prefixIcon: Icon(Icons.credit_card),
+                  helperText: 'ระบุเลขประชาชน 13 หลัก',
+                  hintText: 'xxxxxxxxxxxxx'),
+            ),
+            TextFormField(
+              decoration: InputDecoration(
+                  labelText: 'PINCODE',
+                  border: InputBorder.none,
+                  prefixIcon: Icon(Icons.keyboard),
+                  helperText: 'ระบุ PINCODE 7 หลัก',
+                  hintText: 'xxxxxxx'),
+            ),
+            TextFormField(
+              decoration: InputDecoration(
+                labelText: 'TELEPHONE',
+                border: InputBorder.none,
+                prefixIcon: Icon(Icons.phone),
+                helperText: 'หมายเลขโทรศัพท์',
+              ),
+            ),
+            RaisedButton.icon(
+              icon: Icon(Icons.save),
+              label: Text('REGISTER'),
+              onPressed: () {},
+            )
+          ],
+        ),
       ),
     );
   }
