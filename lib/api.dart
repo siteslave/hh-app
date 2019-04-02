fec5c7b09056c9d3a4049ba960748645d3e1212f
Satit Riapit <siteslave@msn.com>
2019-04-02 16:19:23 +0700

    update

---
+++ lib/api.dart
@@ -0,0 +1,20 @@
+import 'package:http/http.dart' as http;
+
+class Api {
+  Api();
+
+  String apiUrl = 'http://localhost:3000';
+
+  Future doRegister(String cid, String pincode, String telephone) async {
+    // localhost:3000/user/save - POST : pincode, cid, telephone, emi
+    String _url = '$apiUrl/user/save';
+
+    var body = {
+      "cid": cid.toString(),
+      "pincode": pincode.toString(),
+      "telephone": telephone.toString()
+    };
+
+    return await http.post(_url, body: body);
+  }
+}
