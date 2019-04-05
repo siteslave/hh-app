96448a73c798aecd457590233931dfacddfc45de
Satit Riapit <siteslave@msn.com>
2019-04-05 13:55:30 +0700

    update

--- lib/chart.dart
+++ lib/chart.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:charts_flutter/flutter.dart' as charts;
+import 'package:flutter_sparkline/flutter_sparkline.dart';
 
 class ChartPage extends StatefulWidget {
   @override
@@ -23,6 +24,8 @@ class _ChartPageState extends State<ChartPage> {
         data: barchartData,
         labelAccessorFn: (BarChartData sales, _) => '${sales.total.toString()}')
   ];
+
+  var data = [0.0, 1.0, 1.5, 2.0, 0.0, 0.0, -0.5, -1.0, -0.5, 0.0, 0.0];
 
   @override
   Widget build(BuildContext context) {
@@ -51,6 +54,21 @@ class _ChartPageState extends State<ChartPage> {
                           labelPosition: charts.ArcLabelPosition.outside)
                     ])),
           ),
+          new Container(
+//        width: 300.0,
+            height: 100.0,
+            child: new Sparkline(
+              lineColor: Colors.green[900],
+              lineWidth: 5,
+              data: data,
+              fillMode: FillMode.below,
+              fillGradient: new LinearGradient(
+                begin: Alignment.topCenter,
+                end: Alignment.bottomCenter,
+                colors: [Colors.green[500], Colors.green[100]],
+              ),
+            ),
+          )
         ],
       ),
     );
